8a15270c3942057796abcf08983eab307ff66fb6
Kashish-web09 <knarang0909@gmail.com>
2026-01-04 19:11:18 +0530

    Update shoehub.js

--- shoehub.js
+++ shoehub.js
@@ -18,3 +18,40 @@ const slides = document.querySelector(".slides");
 
     slides.style.transform = `translateX(-${currentIndex * 100}%)`;
   }, 3000);
+
+  const currentYear=document.getElementById("currentYear");
+  currentYear.innerHTML=new Date().getFullYear();
+
+  const searchEl=document.getElementById("search-icon");
+  const searchBox=document.querySelector(".search-box");
+
+  searchEl.addEventListener('click',()=>{
+    if(searchBox.querySelector("input"))
+      return;
+    const createInput=document.createElement("input");
+    createInput.classList.add("search-input");
+    createInput.type="text";
+    createInput.placeholder="Search...";
+        searchEl.style.display="none";
+
+    searchBox.appendChild(createInput);
+    createInput.focus();
+
+    createInput.addEventListener('blur',()=>{
+      createInput.remove();
+      searchEl.style.display="inline-block";
+    })
+  });
+
+  const arrowbtnEl=document.getElementById("arrow-btn");
+  const signupemailEl=document.getElementById("signup-email");
+
+  arrowbtnEl.addEventListener('click',(e)=>{
+
+
+    if(signupemailEl.value.trim()===""){
+      alert("Enter the email address");
+    }else{
+      alert("Login Successfully!");
+    }
+  })
